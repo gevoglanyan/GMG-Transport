694ee67b2d3f409f25f9a41a8af7a2659a480d95
gevoglanyan <159603975+gevoglanyan@users.noreply.github.com>
2025-05-28 19:29:04 -0700

    Added Logo & Name

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>GMG Transportation Inc</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Luxury transportation services in Los Angeles" />
+        <link rel="icon" href="/images/GMG.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
+
